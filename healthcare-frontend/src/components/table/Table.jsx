28d07eed5a9f626a178c0c00f6133abb24148ef6
hibaHAJAJI <wiamhiba08@gmail.com>
2026-07-16 16:40:14 +0100

    add Table component with styles for responsive layout

---
+++ healthcare-frontend/src/components/table/Table.jsx
@@ -0,0 +1,20 @@
+import './Table.css';
+
+export const Table = ({ headers, children }) => {
+  return (
+    <div className="table-responsive">
+      <table className="custom-table">
+        <thead>
+          <tr>
+            {headers.map((header, index) => (
+              <th key={index}>{header}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {children}
+        </tbody>
+      </table>
+    </div>
+  );
+};
