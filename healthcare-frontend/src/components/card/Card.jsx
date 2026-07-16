11f69c87848f798c2bd487735227a08fea31dc82
hibaHAJAJI <wiamhiba08@gmail.com>
2026-07-16 16:24:07 +0100

    add Card component with styles for custom card layout

---
+++ healthcare-frontend/src/components/card/Card.jsx
@@ -0,0 +1,17 @@
+import './Card.css';
+
+export const Card = ({ children, title, subtitle, extraClass = '' }) => {
+  return (
+    <div className={`custom-card ${extraClass}`}>
+      {(title || subtitle) && (
+        <div className="card-header">
+          {title && <h3 className="card-title">{title}</h3>}
+          {subtitle && <p className="card-subtitle">{subtitle}</p>}
+        </div>
+      )}
+      <div className="card-body">
+        {children}
+      </div>
+    </div>
+  );
+};
