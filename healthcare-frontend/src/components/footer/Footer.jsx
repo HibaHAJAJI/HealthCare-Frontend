19bc078ef3df3dadbebb5e78019948f0ea72cbf3
hibaHAJAJI <wiamhiba08@gmail.com>
2026-07-16 16:50:05 +0100

    add Footer component with styles for layout and responsiveness

---
+++ healthcare-frontend/src/components/footer/Footer.jsx
@@ -0,0 +1,18 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>
+        © {new Date().getFullYear()} <strong>HealthCare+</strong>. Tous droits
+        réservés.
+      </p>
+
+      <p className="footer-version">
+        Version 1.0.0
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
