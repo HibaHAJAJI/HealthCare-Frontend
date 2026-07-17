4e2d0994fb770ec0b4ec14e7f52bd6d3b56b7f16
hibaHAJAJI <wiamhiba08@gmail.com>
2026-07-17 10:17:45 +0100

    add PatientService with CRUD operations for patient management

---
+++ healthcare-frontend/src/services/patientService.js
@@ -0,0 +1,30 @@
+import API from './axiosConfig';
+
+const patientService = {
+  getAll: async () => {
+    const response = await API.get('/patients');
+    return response.data;
+  },
+
+  getById: async (id) => {
+    const response = await API.get(`/patients/${id}`);
+    return response.data;
+  },
+
+  create: async (patientData) => {
+    const response = await API.post('/patients', patientData);
+    return response.data;
+  },
+
+  update: async (id, patientData) => {
+    const response = await API.put(`/patients/${id}`, patientData);
+    return response.data;
+  },
+
+  delete: async (id) => {
+    const response = await API.delete(`/patients/${id}`);
+    return response.data;
+  }
+};
+
+export default patientService;
