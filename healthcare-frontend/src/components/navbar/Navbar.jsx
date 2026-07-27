56499d26e530d79cde9bf78011caec1af01d25b4
hibaHAJAJI <wiamhiba08@gmail.com>
2026-07-27 16:30:54 +0100

    add user profile link to navbar and improve layout

--- healthcare-frontend/src/components/navbar/Navbar.jsx
+++ healthcare-frontend/src/components/navbar/Navbar.jsx
@@ -3,15 +3,22 @@ import { FaHeartbeat } from "react-icons/fa";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const user = JSON.parse(localStorage.getItem("user"));
+
   return (
     <nav className="navbar">
       <div className="navbar-left">
         <Link to="/dashboard" className="navbar-brand">
           <FaHeartbeat className="health-icon" />
-
           <span className="brand-name">
             HealthCare<span className="brand-plus">+</span>
           </span>
+        </Link>
+      </div>
+
+      <div className="navbar-right">
+        <Link to="/profile" className="profile-btn">
+          {user?.username}
         </Link>
       </div>
     </nav>
