2762039436cafaf3950a0151697da3cc74d028e7
hibaHAJAJI <wiamhiba08@gmail.com>
2026-07-29 23:48:41 +0100

    Add ToastContainer for notifications in the App component

--- healthcare-frontend/src/App.jsx
+++ healthcare-frontend/src/App.jsx
@@ -2,10 +2,23 @@ import "./App.css";
 import AppRoutes from "./routes/AppRoutes";
 import { AuthProvider } from "./context/AuthProvider";
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 function App() {
   return (
     <AuthProvider>
       <AppRoutes />
+
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        closeOnClick
+        pauseOnHover
+        draggable
+        theme="light"
+      />
     </AuthProvider>
   );
 }
