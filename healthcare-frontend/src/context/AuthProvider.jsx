9542da72e248bb15c9691a779146f2415be48ae9
hibaHAJAJI <wiamhiba08@gmail.com>
2026-07-31 12:39:01 +0100

    refactor AuthProvider to simplify token and user state initialization

--- healthcare-frontend/src/context/AuthProvider.jsx
+++ healthcare-frontend/src/context/AuthProvider.jsx
@@ -2,48 +2,23 @@ import { useState } from "react";
 import { AuthContext } from "./AuthContext";
 
 export const AuthProvider = ({ children }) => {
+  const [token, setToken] = useState(
+    localStorage.getItem("token")
+  );
 
-  const [token, setToken] = useState(() => {
-    try {
-      const savedToken = localStorage.getItem("token");
-      return savedToken
-        ? savedToken.replace(/^"|"$/g, "")
-        : null;
-    } catch {
-      return null;
-    }
-  });
-
-
-  const [user, setUser] = useState(() => {
-    try {
-      const savedUser = localStorage.getItem("user");
-      return savedUser
-        ? JSON.parse(savedUser)
-        : null;
-    } catch {
-      return null;
-    }
-  });
-
+  const [user, setUser] = useState(
+    JSON.parse(localStorage.getItem("user")) || null
+  );
 
   const login = (userData, jwtToken) => {
-
-    const cleanToken = jwtToken.replace(/^"|"$/g, "");
-
     setUser(userData);
-    setToken(cleanToken);
+    setToken(jwtToken);
 
-    localStorage.setItem("token", cleanToken);
-    localStorage.setItem(
-      "user",
-      JSON.stringify(userData)
-    );
+    localStorage.setItem("token", jwtToken);
+    localStorage.setItem("user", JSON.stringify(userData));
   };
 
-
   const logout = () => {
-
     setUser(null);
     setToken(null);
 
@@ -53,14 +28,13 @@ export const AuthProvider = ({ children }) => {
     window.location.href = "/login";
   };
 
-
   return (
     <AuthContext.Provider
       value={{
         user,
         token,
         login,
-        logout
+        logout,
       }}
     >
       {children}
